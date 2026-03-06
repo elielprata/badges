f0f7e4cfb5fb8df3d1a54e6285d2de5b7edc11f4
Samuel <samuelbbrandao227@gmail.com>
2026-03-06 17:38:52 -0300

    [FIX] - Adicionado endpoint de impressão dos crachás dos voluntários

--- app/src/main/java/com/emartins/icmbadges/services/api/ApiClient.kt
+++ app/src/main/java/com/emartins/icmbadges/services/api/ApiClient.kt
@@ -1,24 +1,18 @@
 package com.emartins.icmbadges.services.api
 
-import com.emartins.icmbadges.models.ApiResponse
 import com.emartins.icmbadges.models.EnrollmentData
 import com.emartins.icmbadges.models.GetBadgeRequest
 import com.emartins.icmbadges.models.GetEventsResponse
-import com.emartins.icmbadges.models.GetEventsWrapper
 import com.emartins.icmbadges.models.GetLinkResponse
 import com.emartins.icmbadges.models.GetModuleResponse
 import com.emartins.icmbadges.models.SignInRequest
 import com.emartins.icmbadges.models.SignInResponse
-import retrofit2.Response
 import okhttp3.ResponseBody
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.Header
-import retrofit2.http.POST
-import retrofit2.http.Path
-import retrofit2.http.Query
+import retrofit2.Response
+import retrofit2.http.*
 
 interface ApiClient {
+
     @GET("mgestao/api/eventos/inscricoes/list/{eventId}")
     suspend fun searchEnrollment(
         @Path("eventId") eventId: Int,
@@ -26,35 +20,48 @@ interface ApiClient {
         @Query("status") status: String = "A"
     ): List<EnrollmentData>
 
+
     @GET("mgestao/api/evento/trabalhadores/list/{eventId}")
     suspend fun searchVoluntaryEnrollment(
         @Path("eventId") eventId: Int,
         @Query("kfilter") query: String
     ): List<EnrollmentData>
 
+
+    // crachá seminaristas
     @POST("mgestao/api/eventos/inscricoes/printcrachas/{eventId}")
     suspend fun getBadgePdf(
         @Path("eventId") eventId: Int,
         @Body body: GetBadgeRequest
     ): Response<ResponseBody>
 
+
+    // crachá trabalhadores
+    @POST("mgestao/api/evento/trabalhadores/printcrachas/{eventId}")
+    suspend fun getWorkerBadgePdf(
+        @Path("eventId") eventId: Int,
+        @Body body: GetBadgeRequest
+    ): Response<ResponseBody>
+
+
     @POST("v2/api/entrar")
     suspend fun signIn(
         @Body body: SignInRequest
     ): SignInResponse
+
 
     @GET("v2/api/obter-url-v1?ModuloId=7")
     suspend fun getLink(
         @Header("Authorization") token: String
     ): GetLinkResponse
 
+
     @GET("user/modules/7")
-    suspend fun getModuleInfo(
-    ): List<GetModuleResponse>
+    suspend fun getModuleInfo(): List<GetModuleResponse>
 
 
     @GET("/mgestao/api/eventos/list/{codNivel}")
     suspend fun getEvents(
-        @Path("codNivel") codNivel: String,
+        @Path("codNivel") codNivel: String
     ): List<GetEventsResponse>
 }
