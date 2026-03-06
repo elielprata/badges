fa53d68fc1b64e04023b39922e37121c810dda84
Samuel <samuelbbrandao227@gmail.com>
2026-03-06 17:39:42 -0300

    [FIX] - Correção da busca pelo nome do participante

--- app/src/main/java/com/emartins/icmbadges/models/EnrollmentData.kt
+++ app/src/main/java/com/emartins/icmbadges/models/EnrollmentData.kt
@@ -1,13 +1,22 @@
 package com.emartins.icmbadges.models
 
+import com.google.gson.annotations.SerializedName
+
 data class EnrollmentDataWrapper(
     val total: Int,
     val items: List<EnrollmentData>
 )
 
 data class EnrollmentData(
+
+    @SerializedName("isem_id")
     val isem_id: String,
-    val nome: String,
-    val dsc_classe: String,
-    val nom_igreja: String
+
+    // pode vir como "nome" (seminarista) ou "nom_membro" (voluntário)
+    @SerializedName(value = "nome", alternate = ["nom_membro"])
+    val nome: String? = null,
+
+    val dsc_classe: String? = null,
+
+    val nom_igreja: String? = null
 )
